93a354031f0631024e1bde5b352b733a667da147
baudoin <baudoinvicbolingo@gmail.com>
2024-09-18 10:07:45 +0200

    deleted uncessary code

--- src/layouts/ReviewCard.jsx
+++ src/layouts/ReviewCard.jsx
@@ -4,11 +4,7 @@ const ReviewCard = (props) => {
   return (
     <div className=" w-full md:w-1/3 bg-white border-2 border-lightText md:border-none p-5 rounded-lg shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]">
       <div>
-        <p className=" text-lightText">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. In
-          consectetur error, dolores quae ipsa quos enim corporis magni
-          obcaecati tempore natus eos, 
-        </p>
+      
       </div>
 
       <div className=" flex flex-row justify-center items-center mt-4 gap-4">
