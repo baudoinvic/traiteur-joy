c60d2603a06531e3c6fa5925e4f872b897ee0449
baudoin <baudoinvicbolingo@gmail.com>
2024-09-20 23:40:48 +0200

    unistall axios

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,6 +7,7 @@ import { Link } from "react-router-dom";
 import funy from "../assets/img/funy.jpg";
 import { useTranslation } from "react-i18next";
 
+
 const Home = () => {
     const { t, i18n } = useTranslation(); 
   return (
